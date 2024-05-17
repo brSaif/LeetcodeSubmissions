63bbb25b7f4f90314df352e6c166b974f4155dd9
brSaif <saifeddine.benromdhane@protonmail.com>
2024-05-17 22:29:57 +0100

    Removed invalid submissions

--- submissions/189-rotate-array/2021-06-07 01.44.27 - Runtime Error - runtime NA - memory NA.cs
+++
@@ -1,9 +0,0 @@
-public class Solution {
-    public void Rotate(int[] nums, int k) {
-        if(nums.Length > 1){
-        Array.Reverse(nums);
-        Array.Reverse(nums,0,k);
-        Array.Reverse(nums,k,nums.Length - k );
-        }
-    }
-}
--- submissions/326-power-of-three/2021-06-13 16.06.23 - Runtime Error - runtime NA - memory NA.cs
+++
@@ -1,9 +0,0 @@
-public class Solution {
-    public bool IsPowerOfThree(int n) {
-        // First Approach
-        // this has a precision bug it returns 0.99999999 instead of 1 for 243 like cases 
-        // var res = Math.Log(n,3) % 1;
-        // return res == 0 ; 
-        return 1162261467 % n == 0;
-    }
-}
